7a3093904df6d0d662cfa6ad4fa12666d92b13ea
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-10-18 15:37:10 +0200

    add-contact-link

--- la-mia-pizzeria-post/Program.cs
+++ la-mia-pizzeria-post/Program.cs
@@ -34,7 +34,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Contact}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 
